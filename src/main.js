7aa52a748f9dccd720e0871fddbc824fa7925989
Kenneth <kennethyoung.2960@gmail.com>
2024-03-24 18:43:25 -0500

    Corrected errors

--- src/main.js
+++ src/main.js
@@ -1,28 +1,11 @@
 const arrayA = [1, 3, 3, 6, 6, 7, 9];
 const arrayB = [2, 3, 5, 7];
 
-function excludeIfInBoth(array) {
+function excludeIfInBoth(arrA, arrB) {
   const result = [];
 
-  for (let i = 0; i < arrayA.length; i++)
   return result;
 }
 
 module.exports = excludeIfInBoth;
 
-//This attempt was only returning single unique values even if it appeared multiples times in the array
-//   let foundNumbers = {};
-
-//   for (let num of arrayA) {
-//     if(foundNumbers[num])
-//   }
-
-//   for (let num of arrayB) {
-//     if (foundNumbers.includes(num)) { //.has works on objects not arrays so switch to array
-//         foundNumbers.splice();
-//     } else {
-
-//     }
-//   }
-
-//   return foundNumbers;
--- test/main.test.js
+++ test/main.test.js
@@ -1,15 +1,16 @@
-const excludeIfInBoth = require('../src/main')
+const assert = require('assert')
+const excludeIfInBoth = require("../src/main");
 
-const testInputA = [ 1 , 3 , 3 , 6 , 6 , 7 , 9 ];
-const testinputB = [ 2 , 3 , 5 , 7 ]
-const expectedResult = [ 1, 2 , 5 , 6 , 6 , 9 ];
 
-const result = excludeIfInBoth(testInputA, testinputB)
+describe("excludeIfInBoth function", function () {
+  it(
+    "should exclude elements if they are found in testInputA as well as testInputB"
+  );
+  const testInputA = [1, 3, 3, 6, 6, 7, 9];
+  const testinputB = [2, 3, 5, 7];
+  const expectedResult = [1, 2, 5, 6, 6, 9];
 
-if (JSON.stringify(result) === JSON.stringify(expectedResult)) {
-    console.log('Test Passed!');
-  } else {
-    console.error('Test Failed!');
-    console.log('Expected: ', expectedResult);
-    console.log('Actual: ', result);
-  }
+  const result = excludeIfInBoth(testInputA, testinputB);
+
+  assert.deepStrictEqual(result, expectedResult);
+});
