671f2ac8805eb61b82339bca69e22c50a6277e18
Kenneth <kennethyoung.2960@gmail.com>
2024-03-24 02:13:23 -0500

    reset try different method

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 const arrayA = [1, 3, 3, 6, 6, 7, 9];
 const arrayB = [2, 3, 5, 7];
 
-function excludeIfInBoth(firstArray, secondArray) {
-  let foundNumbers = [];
+function excludeIfInBoth(array) {
+  const result = [];
 
-  for (let num of arrayA) {
-    foundNumbers.push(num);
-  }
-  // We have added in all the numbers of the first array
-  // We want to compare arrayB to remove any numbers found in both A and B
-  // So if found in A and B, exlude from C
-
-  return foundNumbers; // Will need to change output from the array or set of foundNumbers
-  //  Instead we will need to return arrayC
+  for (let i = 0; i < arrayA.length; i++)
+  return result;
 }
 
 module.exports = excludeIfInBoth;
+
+//This attempt was only returning single unique values even if it appeared multiples times in the array
+//   let foundNumbers = {};
+
+//   for (let num of arrayA) {
+//     if(foundNumbers[num])
+//   }
+
+//   for (let num of arrayB) {
+//     if (foundNumbers.includes(num)) { //.has works on objects not arrays so switch to array
+//         foundNumbers.splice();
+//     } else {
+
+//     }
+//   }
+
+//   return foundNumbers;
