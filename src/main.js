25ab96d2cae2aa5a4c97643caba3837f5ee39a1c
Kenneth <kennethyoung.2960@gmail.com>
2024-03-24 19:18:27 -0500

    Returning values from both arrays. Check logic to remove extra single number found in both

--- src/main.js
+++ src/main.js
@@ -3,9 +3,36 @@ const arrayB = [2, 3, 5, 7];
 
 function excludeIfInBoth(arrA, arrB) {
   const result = [];
+  const countA = new Map(); //Map() should help to have each number counted in the final array
+  const countB = new Map(); //Because each value will be treated as a key-value pair in the map
+
+  for (const num of arrA) {countA.set(num, (countA.get(num) || 0) + 1);}
+  // count each number in arrA and add to map countA
+
+  for (const num of arrB) {countB.set(num, (countB.get(num) || 0) + 1);}
+  // count each number in arrB and add to map countB
+    
+  // Now we need to compare A to B and push (A-(A&&B)) into C
+  for (const num of arrA) {
+    if (!countB.has(num) || countB.get(num) === 0) {
+      result.push(num);
+    } else {
+      countB.set(num, countB.get(num) - 1);
+    }
+  }
+  // In this block we compare B to A and push (B-(B&&A)) into C. Hopefully.
+  for (const num of arrB) {
+    if (!countA.has(num) || countA.get(num) === 0) {
+        result.push(num);
+    } else {
+        countA.set(num, countA.get(num) - 1);
+    }
+  }
 
   return result;
 }
 
-module.exports = excludeIfInBoth;
+const expectedResult = excludeIfInBoth(arrayA, arrayB);
+console.log(expectedResult); //ensure mathching with mocha test results
 
+module.exports = excludeIfInBoth;
