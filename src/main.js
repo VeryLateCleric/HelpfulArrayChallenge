2e02054025b71cacf8c9c380e004d951461f0d56
Kenneth <kennethyoung.2960@gmail.com>
2024-03-24 20:31:19 -0500

    Written to save space and make Trevor upset. 2 of 2 passing

--- src/main.js
+++ src/main.js
@@ -3,30 +3,11 @@ const arrayB = [2, 3, 5, 7];
 
 function excludeIfInBoth(arrA, arrB) {
   const result = [];
-  const countA = new Map(); //Map() should help to have each number counted in the final array
-  const countB = new Map(); //Because each value will be treated as a key-value pair in the map
+  const commonNumbers = new Set();
 
-  for (const num of arrA) {countA.set(num, (countA.get(num) || 0) + 1);}
-  for (const num of arrB) {countB.set(num, (countB.get(num) || 0) + 1);}
+  for (const num of arrA) if (arrB.includes(num)) {commonNumbers.add(num)} else {result.push(num)}
 
-  for (const num of arrA) {
-    if (!countB.has(num) || countB.get(num) === 0) {
-      result.push(num);
-    } else {
-      countB.set(num, countB.get(num) - 1);
-      if (countA.get(num) > 0) {
-        countA.set(num, countA.get(num) - 1);
-      }
-    } 
-  }
-
-  for (const num of arrB) {
-    if (!countA.has(num) || countA.get(num) === 0) {
-        result.push(num);
-    } else {
-        countA.set(num, countA.get(num) - 1);
-    }
-  }
+  for (const num of arrB) if (arrA.includes(num)) {commonNumbers.add(num)} else {result.push(num)}
 
   result.sort((a, b) => a - b); //Gotta be in order cause I can't read
 
@@ -34,6 +15,5 @@ function excludeIfInBoth(arrA, arrB) {
 }
 
 const expectedResult = excludeIfInBoth(arrayA, arrayB);
-console.log(expectedResult); //ensure mathching with mocha test results
-
+console.log(expectedResult); //ensure matchhing with mocha test results
 module.exports = excludeIfInBoth;
